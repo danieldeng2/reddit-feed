af972094937835c4a812102d60e7c3a4e98c868f
danieldeng2 <danieldeng223@gmail.com>
2021-01-09 18:41:17 +0000

    Update tests to include situations with no arguments

--- tests/services.test.js
+++ tests/services.test.js
@@ -15,6 +15,32 @@ describe("Test the fetchArticles service", () => {
 			);
 	});
 
+	it("parses responses correctly with timeframe unspecified", () => {
+		return new FetchArticles()
+			.run("correctSubreddit", undefined, 25)
+			.then(
+				data => {
+					expect(data.subreddit).toBe("correctSubreddit");
+					expect(data.articles.length).toBe(2);
+					expect(data.articles[0].title).toBe("first article");
+					expect(data.articles[1].id).toBe("abcde");
+				}
+			);
+	});
+
+	it("parses responses correctly with limit unspecified", () => {
+		return new FetchArticles()
+			.run("correctSubreddit", "day", undefined)
+			.then(
+				data => {
+					expect(data.subreddit).toBe("correctSubreddit");
+					expect(data.articles.length).toBe(2);
+					expect(data.articles[0].title).toBe("first article");
+					expect(data.articles[1].id).toBe("abcde");
+				}
+			);
+	});
+
 	it("fails gracefully if no subreddit name provided", () => {
 		const fetchArticles = new FetchArticles().run(null, "day", 25);
 		return expect(fetchArticles)
